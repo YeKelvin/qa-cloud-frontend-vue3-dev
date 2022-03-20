2f9da5450ded38f04092fc94eed6d710783196ff
Kelvin.Ye <testmankelvin@163.com>
2022-03-20 22:57:23 +0800

    update

--- src/components/monaco-editor/monaco.base.js
+++ src/components/monaco-editor/monaco.base.js
@@ -22,9 +22,9 @@ import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js'
 
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
 import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+// import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
 import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+// import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
 import { LOG_LANGUAGE_DEFINE, LOG_TOKEN_COLORS } from './language/log'
 import { PYMETER_COMPLETION_PROVIDER, PYMETER_FUNCTION_COMPLETION_PROVIDER } from './language/pymeter'
@@ -35,15 +35,15 @@ self.MonacoEnvironment = {
     if (label === 'json') {
       return new jsonWorker()
     }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new cssWorker()
-    }
+    // if (label === 'css' || label === 'scss' || label === 'less') {
+    //   return new cssWorker()
+    // }
     if (label === 'html' || label === 'handlebars' || label === 'razor') {
       return new htmlWorker()
     }
-    if (label === 'typescript' || label === 'javascript') {
-      return new tsWorker()
-    }
+    // if (label === 'typescript' || label === 'javascript') {
+    //   return new tsWorker()
+    // }
     return new editorWorker()
   }
 }
--- vite.config.js
+++ vite.config.js
@@ -50,9 +50,9 @@ export default ({ mode }) =>
             vue: ['vue', 'vuex', 'vue-router'],
             echarts: ['echarts'],
             jsonWorker: ['monaco-editor/esm/vs/language/json/json.worker'],
-            cssWorker: ['monaco-editor/esm/vs/language/css/css.worker'],
+            // cssWorker: ['monaco-editor/esm/vs/language/css/css.worker'],
             htmlWorker: ['monaco-editor/esm/vs/language/html/html.worker'],
-            tsWorker: ['monaco-editor/esm/vs/language/typescript/ts.worker'],
+            // tsWorker: ['monaco-editor/esm/vs/language/typescript/ts.worker'],
             editorWorker: ['monaco-editor/esm/vs/editor/editor.worker']
           }
         }
@@ -75,9 +75,9 @@ export default ({ mode }) =>
     optimizeDeps: {
       include: [
         'monaco-editor/esm/vs/language/json/json.worker',
-        'monaco-editor/esm/vs/language/css/css.worker',
+        // 'monaco-editor/esm/vs/language/css/css.worker',
         'monaco-editor/esm/vs/language/html/html.worker',
-        'monaco-editor/esm/vs/language/typescript/ts.worker',
+        // 'monaco-editor/esm/vs/language/typescript/ts.worker',
         'monaco-editor/esm/vs/editor/editor.worker'
       ]
     }
